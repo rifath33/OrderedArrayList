895032e4a1e21952498bfa744603336522f99566
Rifath <hrifath20@stuy.edu>
2020-12-14 08:07:19 -0500

    got add(element) working. I was reminded of the fact that we cna call methods from the parent class using super.(whatever method you want). it just goes to show how useful these class heirarchies are. thank you.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
+
 public class NoNullArrayList<T> extends ArrayList<T>{
 
   public boolean add(T element){
-
+    if(element == null){
+      throw new IllegalArgumentException ("Elements cannot be null.");
+    }
+    else{
+      super.add(element);
+      return(super.get(size()-1)==element);
+    }
   }
 
   public void add(int index, T element){
 
   }
 
-  public T set(int index, T element){
-    
-  }
+  // public T set(int index, T element){
+  //
+  // }
 
 }

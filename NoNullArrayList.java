7e6f6730358ab4c3f58656c6fdad782d015c42e3
Rifath <hrifath20@stuy.edu>
2020-12-14 08:08:42 -0500

    I got add(index, element) working. I used the superclass method again. very useful thing...

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -13,7 +13,12 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   }
 
   public void add(int index, T element){
-
+    if(element == null){
+      throw new IllegalArgumentException ("Elements cannot be null.");
+    }
+    else{
+      super.add(index, element);
+    }
   }
 
   // public T set(int index, T element){

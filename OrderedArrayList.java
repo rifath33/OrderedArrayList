516c619c41dfea20829da2366fd6d8f907484a58
Rifath <hrifath20@stuy.edu>
2020-12-14 08:33:35 -0500

    just added the barebones method signatures for both adds and the set

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,16 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
+  // public boolean add(T element){
+  //
+  // }
+  //
+  // public void add(int index, T element){
+  //
+  // }
+  //
+  // public T set(int index, T element){
+  //
+  // }
+
 }

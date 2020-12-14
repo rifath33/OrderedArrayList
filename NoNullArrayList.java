f09c023dde912239a064c3d733447d627cc988f7
Rifath <hrifath20@stuy.edu>
2020-12-14 07:29:42 -0500

    Okay so I made the three method signatures. I learned that for ArrayIndex, the actual class is ArrayList<E> in the java documentation and for every method, instead of listing a specific type (like string), java's creators put E in place of that. And so for ArrayList<E>, the signature for set is public E set(int index, E element). This all helps me understand generic variable types better.

---
+++ NoNullArrayList.java
@@ -0,0 +1,15 @@
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public boolean add(T element){
+
+  }
+
+  public void add(int index, T element){
+
+  }
+
+  public T set(int index, T element){
+    
+  }
+
+}

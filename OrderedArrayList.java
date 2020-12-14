3f4612687a0022cbd330d46289a49efbba007e3d
Rifath <hrifath20@stuy.edu>
2020-12-14 08:57:07 -0500

    made a findIndex method to help with add methods... hope it works right

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,10 +8,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
+  public int findIndex(T element){
+    int index = 0;
+    int stop = 0;
+    for(int i = 0; i < size() && stop != 1; i++){
+      if(get(i).compareTo(element) < 0)
+      index++;
+      if( (get(i).compareTo(element) > 0) || (get(i).compareTo(element) == 0) )
+      index++;
+      stop++;
+    }
+    return index;
+  }
+
   // public boolean add(T element){
   //
   // }
-  //
+
   // public void add(int index, T element){
   //
   // }

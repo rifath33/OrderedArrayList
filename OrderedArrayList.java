fea6522a93c826443d58533c13e27ed816ae31d5
Rifath <hrifath20@stuy.edu>
2020-12-14 09:27:51 -0500

    I fixed add(element). Basically I'm going to delete my findIndex method it's really bad. Our goal is to either add an element when the thing in the index is less than or equal to 0... or if that isn't the case and everything is larger just add it in the beginning of the list... my index method didn't think that way.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -22,11 +22,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public boolean add(T element){
-    super.add(findIndex(element),element);
-    return(super.get(size()-1).equals(element));
+    if(element == null){
+      throw new IllegalArgumentException ("Elements cannot be null.");
+    }
+    for (int i = 0; i < size(); i++) {
+      if (element.compareTo(this.get(i)) <= 0) {
+        super.add(i, element);
+        return(super.get(size()-1).equals(element));
+      }
+    }
+      super.add(0, element);
+      return(super.get(size()-1).equals(element));
   }
 
   public void add(int index, T element){
+    if(element == null){
+      throw new IllegalArgumentException ("Elements cannot be null.");
+    }
     super.add(findIndex(element),element);
   }
 

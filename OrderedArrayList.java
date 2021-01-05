077fb91462dc493cc5b90d0f814d759198419e7f
rifath33 <hrifath20@stuy.edu>
2021-01-05 04:13:04 -0500

    came up with a better way to find index

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,6 +7,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
+  
+  public int indexOf(T element){
+      int index = 0; 
+      
+      if(super.size() < 1)
+      return 0;
+      else{
+          
+          for(int i = 0; i < super.size(); i++){
+              if(element.compareTo(super.get(i)) >= 0)
+              index = i + 1;
+          }
+          
+      }
+      
+      return index;
+      
+  }
 
   public boolean add(T element){
     if(element == null){
